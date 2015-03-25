ef74853fb18af2b46a2fb3328800e45fff02855c
Virginie Trubiano & Kyle Bulloch <student@epicodus.com>
2015-03-25 10:42:03 -0700

    Add book methods up to updateAuthor

--- src/Book.php
+++ src/Book.php
@@ -22,9 +22,96 @@
         {
             $this->title = (string) $new_title;
         }
+
+        function getAuthor()
+        {
+            return $this->author;
+        }
+
+        function setAuthor ($new_author)
+        {
+            $this->author = (string) $new_author;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId ($new_id)
+        {
+            $this->id = (int) $new_id;
+        }
+
+        function save()
+        {
+            $statement = $GLOBALS['DB']->query("INSERT INTO books (title, author) VALUES ('{$this->getTitle()}', '{$this->getAuthor()}') RETURNING id;");
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $this->setId($result['id']);
+        }
+
+        static function getAll()
+        {
+            $returned_books = $GLOBALS['DB']->query("SELECT * FROM books;");
+            $books = [];
+            foreach($returned_books as $a_book){
+                $title = $a_book['title'];
+                $author = $a_book['author'];
+                $id = $a_book['id'];
+                $new_book = new Book($title, $author, $id);
+                array_push($books, $new_book);
+            }
+            return $books;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM books*;");
+        }
+
+        function updateTitle($new_title)
+        {
+            $GLOBALS['DB']->exec("UPDATE books SET title '{$new_title}' WHERE id = {$this->getId()};");
+            $this->setTitle($new_title);
+        }
+
+        function updateAuthor($new_author)
+        {
+            $GLOBALS['DB']->exec("UPDATE books SET author '{$new_author}' WHERE id = {$this->getId()};");
+            $this->setAuthor($new_author);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-
-
-
-
  ?>
--- tests/BookTest.php
+++ tests/BookTest.php
@@ -7,13 +7,13 @@
 
     require_once "src/Book.php";
 
-//    $DB = new PDO('pgsql:host=localhost;dbname=library_test');
+    $DB = new PDO('pgsql:host=localhost;dbname=library_test');
 
     class BookTest extends PHPUnit_Framework_TestCase
     {
         protected function tearDown()
         {
-            //deleteALl()
+            Book::deleteAll();
         }
 
         function testGetTitle()
@@ -45,6 +45,159 @@
             //Assert
             $this->assertEquals($new_title, $result);
         }
+
+        function testGetAuthor()
+        {
+            //Arrange
+            $title = "Dungeons and Dragons";
+            $author = "James Brown";
+            $test_book = new Book($title, $author);
+
+            //Act
+            $result = $test_book->getAuthor();
+
+            //Assert
+            $this->assertEquals($author, $result);
+        }
+
+        function testSetAuthor()
+        {
+            //Arrange
+            $title = "Dungeons and Dragons";
+            $author = "James Brown";
+            $test_book = new Book($title, $author);
+            $new_author = "Dragons";
+
+            //Act
+            $test_book->setAuthor($new_author);
+            $result = $test_book->getAuthor();
+
+            //Assert
+            $this->assertEquals($new_author, $result);
+        }
+
+        function testGetId()
+        {
+            //Arrange
+            $title = "Dungeons and Dragons";
+            $author = "James Brown";
+            $id = 11;
+            $test_book = new Book($title, $author, $id);
+
+            //Act
+            $result = $test_book->getId();
+
+            //Assert
+            $this->assertEquals($id, $result);
+        }
+
+        function testSetId()
+        {
+            //Arrange
+            $title = "Dungeons and Dragons";
+            $author = "James Brown";
+            $id = 11;
+            $test_book = new Book($title, $author, $id);
+            $new_id = 22;
+
+            //Act
+            $test_book->setId($new_id);
+            $result = $test_book->getId();
+
+            //Assert
+            $this->assertEquals($new_id, $result);
+        }
+
+        function testSave()
+        {
+            //Arrange
+            $title = "Dungeons and Dragons";
+            $author = "James Brown";
+            $test_book = new Book($title, $author);
+            $test_book->save();
+
+            //Act
+            $result = Book::getAll();
+
+            //Assert
+            $this->assertEquals($test_book, $result[0]);
+        }
+
+        function testDeleteAll()
+        {
+            //Arrange
+            $title = "Dungeons and Dragons";
+            $author = "James Brown";
+            $test_book = new Book($title, $author);
+            $test_book->save();
+
+            $title2 = "Antechrist";
+            $author2 = "Amelie Notomb";
+            $test_book2 = new Book($title2, $author2);
+            $test_book2->save();
+
+            //Act
+            Book::deleteAll();
+            $result = Book::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+
+        function testUpdateTitle()
+        {
+            //Arrange
+            $title = "Dungeons and Dragons";
+            $author = "James Brown";
+            $test_book = new Book($title, $author);
+            $test_book->save();
+
+            $new_title = "Experiments in Murder";
+
+            //Act
+            $test_book->updateTitle($new_title);
+
+            //Assert
+            $result = $test_book->getTitle();
+            $this->assertEquals($new_title, $result);
+        }
+
+        function testUpdateAuthor()
+        {
+            //Arrange
+            $title = "Dungeons and Dragons";
+            $author = "James Brown";
+            $test_book = new Book($title, $author);
+            $test_book->save();
+
+            $new_author = "Francis Bacon";
+            
+            //Act
+            $test_book->updateAuthor($new_author);
+
+            //Assert
+            $result = $test_book->getAuthor();
+            $this->assertEquals($new_author, $result);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
